28fac5879367fe2328a686faec3f9a16029d7a33
Lakshya17verma <lakshmanverma17@gmail.com>
2022-10-09 19:51:41 +0530

    new Upload
    newUpload

---
+++ script.js
@@ -0,0 +1,37 @@
+function function1() {
+    document.getElementById("first").style.visibility = 'visible';
+    document.getElementById("fourth").style.visibility = 'hidden';
+}
+
+function function2() {
+    document.getElementById("second").style.visibility = 'visible';
+    document.getElementById("fourth").style.visibility = 'hidden';
+}
+
+function function3() {
+    document.getElementById("third").style.visibility = 'visible';
+    document.getElementById("fourth").style.visibility = 'hidden';
+}
+function close1() {
+    document.getElementById("first").style.visibility = 'hidden';
+
+}
+function close2() {
+    document.getElementById("second").style.visibility = 'hidden';
+}
+function close3() {
+    document.getElementById("third").style.visibility = 'hidden';
+}
+
+function function4() {
+    document.getElementById("fourth").style.visibility = 'visible';
+    document.getElementById("first").style.visibility = 'hidden';
+    document.getElementById("second").style.visibility = 'hidden';
+    document.getElementById("third").style.visibility = 'hidden';
+}
+function close4() {
+    document.getElementById("fourth").style.visibility = 'hidden';
+    document.getElementById("first").style.visibility = 'hidden';
+    document.getElementById("second").style.visibility = 'hidden';
+    document.getElementById("third").style.visibility = 'hidden';
+}
